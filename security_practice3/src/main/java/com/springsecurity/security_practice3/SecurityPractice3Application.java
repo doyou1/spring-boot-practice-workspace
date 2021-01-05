d1a2c852119968c8a4f2cb80d0b16797fa5ed93a
doyou1 <tla149@daum.net>
2021-01-05 18:07:04 +0900

    [성공] SpringSecurity를 이용한 로그인, 회원가입 기능 구현 성공

---
+++ security_practice3/src/main/java/com/springsecurity/security_practice3/SecurityPractice3Application.java
@@ -0,0 +1,13 @@
+package com.springsecurity.security_practice3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPractice3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityPractice3Application.class, args);
+	}
+
+}
