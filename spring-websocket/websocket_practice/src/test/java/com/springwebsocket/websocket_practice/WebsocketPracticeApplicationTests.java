a599a29b36793ac05a6e4f7e0298813093560f38
doyou1 <tla149@daum.net>
2021-01-06 17:41:18 +0900

    [성공]Springboot-Websocket Practice

---
+++ spring-websocket/websocket_practice/src/test/java/com/springwebsocket/websocket_practice/WebsocketPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwebsocket.websocket_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
